9036c00395a1acace26e625766e014245b7ab4f8
Илья Рыбаков <i.rybakov@webpractik.ru>
2024-12-24 20:13:57 +0300

    ref

--- tests/first.spec.ts
+++ tests/first.spec.ts
@@ -5,6 +5,7 @@ import { test, expect } from '@playwright/test';
 test('Add user', async ({ page }) => {
   await page.goto('https://demoqa.com/webtables/');
   await page.getByRole('button', { name: 'Add' }).click();
+
   await expect(page.getByRole('dialog')).toBeVisible();
   await page.getByPlaceholder('First Name').fill('FirstName');
   await page.getByPlaceholder('Last Name').fill('LastName');
@@ -13,31 +14,15 @@ test('Add user', async ({ page }) => {
   await page.getByPlaceholder('Salary').fill('200000');
   await page.getByPlaceholder('Department').fill('QA');
   await page.getByRole('button', { name: 'Submit' }).click();
+
   await expect(page.getByRole('dialog')).not.toBeVisible();
   await expect(page.locator('.rt-tbody').getByRole('row' , { name: 'FirstName' })).toHaveText('FirstNameLastName30mytestemail@mail.ru200000QA ');
 });
 
-test('Delete user', async ({ page }) => {
-  await page.goto('https://demoqa.com/webtables/');
-  await page.locator('.rt-tbody').getByRole('row' , { name: 'Cierra' }).getByTitle('Delete').click();
-  await expect(page.locator('.rt-tbody').getByRole('row' , { name: 'Cierra' })).not.toBeVisible();
-});
-
-test('Add user with different data', async ({ page }) => {
+test('Add user 2', async ({ page }) => {
   await page.goto('https://demoqa.com/webtables/');
   await page.getByRole('button', { name: 'Add' }).click();
-  await expect(page.getByRole('dialog')).toBeVisible();
-  await page.getByPlaceholder('First Name').fill('FirstName');
-  await page.getByPlaceholder('Last Name').fill('LastName');
-  await page.getByPlaceholder('name@example.com').fill('mytestemail@mail.ru');
-  await page.getByPlaceholder('Age').fill('30');
-  await page.getByPlaceholder('Salary').fill('200000');
-  await page.getByPlaceholder('Department').fill('QA');
-  await page.getByRole('button', { name: 'Submit' }).click();
-  await expect(page.getByRole('dialog')).not.toBeVisible();
-  await expect(page.locator('.rt-tbody').getByRole('row' , { name: 'FirstName' })).toHaveText('FirstNameLastName30mytestemail@mail.ru200000QA ');
 
-  await page.getByRole('button', { name: 'Add' }).click();
   await expect(page.getByRole('dialog')).toBeVisible();
   await page.getByPlaceholder('First Name').fill('FirstName2');
   await page.getByPlaceholder('Last Name').fill('LastName2');
@@ -46,18 +31,15 @@ test('Add user with different data', async ({ page }) => {
   await page.getByPlaceholder('Salary').fill('200001');
   await page.getByPlaceholder('Department').fill('QA2');
   await page.getByRole('button', { name: 'Submit' }).click();
+  
   await expect(page.getByRole('dialog')).not.toBeVisible();
   await expect(page.locator('.rt-tbody').getByRole('row' , { name: 'FirstName2' })).toHaveText('FirstName2LastName232mytestemail2@mail.ru200001QA2 ');
-
-  await page.getByRole('button', { name: 'Add' }).click();
-  await expect(page.getByRole('dialog')).toBeVisible();
-  await page.getByPlaceholder('First Name').fill('FirstName3');
-  await page.getByPlaceholder('Last Name').fill('LastName3');
-  await page.getByPlaceholder('name@example.com').fill('mytestemail3@mail.ru');
-  await page.getByPlaceholder('Age').fill('33');
-  await page.getByPlaceholder('Salary').fill('200003');
-  await page.getByPlaceholder('Department').fill('QA3');
-  await page.getByRole('button', { name: 'Submit' }).click();
-  await expect(page.getByRole('dialog')).not.toBeVisible();
-  await expect(page.locator('.rt-tbody').getByRole('row' , { name: 'FirstName3' })).toHaveText('FirstName3LastName333mytestemail3@mail.ru200003QA3 ');
 });
+
+test('Delete user', async ({ page }) => {
+  await page.goto('https://demoqa.com/webtables/');
+  await page.locator('.rt-tbody').getByRole('row' , { name: 'Cierra' }).getByTitle('Delete').click();
+  await expect(page.locator('.rt-tbody').getByRole('row' , { name: 'Cierra' })).not.toBeVisible();
+});
+
+
